c5e4e9fd8bbbb0feeab248183858c055a3c72853
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2024-04-11 21:02:14 +1000

    updates

--- scripts/utils_jax.py
+++ scripts/utils_jax.py
@@ -147,7 +147,70 @@ def bitstring_to_state(bitstring:str):
   return tensor_product(*single_qubit_states)
 
 
+# utilities for the flipped quanvolution kernel
+def extract_patches(image, patch_size, stride, padding=None):
+    """
+    Extracts patches from an image with multiple input channels and optional custom padding.
 
+    Args:
+        image (jnp.ndarray): Input image tensor of shape (in_channels, height, width).
+        patch_size (int): Size of the square patches to extract.
+        stride (int): Stride between patches.
+        padding (tuple): Padding value(s) for each dimension.
+
+    Returns:
+        jnp.ndarray: Tensor of extracted patches of shape (num_patches, in_channels, patch_size, patch_size).
+    """
+
+    in_channels, height, width = image.shape
+
+    pad_h, pad_w = padding if padding is not None else (0, 0)
+
+
+    image = jnp.pad(image, [(0, 0), (pad_h, pad_h), (pad_w, pad_w)], mode='constant') if padding is not None else image
+
+
+    _, height, width = image.shape
+
+
+    num_patches_h = (height - patch_size) // stride + 1
+    num_patches_w = (width - patch_size) // stride + 1
+
+    patch_indices = [(i, j) for i in range(num_patches_h) for j in range(num_patches_w)]
+
+    patches = jnp.stack([image[:, i*stride:i*stride+patch_size, j*stride:j*stride+patch_size]
+                         for i, j in patch_indices])
+
+    return patches
+
+
+def generate_2q_param_state(theta):
+  state = bitstring_to_state('00')
+  state = jnp.dot(
+      su4_op(theta),
+      state
+  )
+  return state
+
+vmap_generate_2q_param_state = jax.vmap(generate_2q_param_state, in_axes=0, out_axes = 0)
+
+def single_kernel_op(thetas, patch):
+  # patch has shape (c_in, h, w)
+  # thetas has shape (c_in, 4^2-1) for SU4 gates
+  n_theta = thetas.shape[0]
+  n_channel = patch.shape[0]
+  assert n_theta == n_channel, "Thetas and patch must have the same number of channels."
+  states = vmap_generate_2q_param_state(thetas)
+  patch = jnp.pad(patch, [(0,0),(0,1),(0,1)], mode='constant')
+  herm_patch = (jnp.einsum("ijk->ikj", patch)+patch)/2
+  channel_out = vmap_measure_sv_ob_pairs([states, herm_patch])
+  return jnp.sum(channel_out, axis = 0)/n_theta
+
+vmap_single_kernel_op_through_extracted_patches = jax.vmap(single_kernel_op, in_axes=(None, 0), out_axes=0)
+
+# For multiple channel output
+# parameter has shape (c_out, c_in, 4**2-1) for SU4 gates
+vmap_vmap_single_kernel_op_through_extracted_patches = jax.vmap(vmap_single_kernel_op_through_extracted_patches, in_axes=(0, None), out_axes=0)
 
 
 if __name__ == "__main__":
@@ -186,3 +249,31 @@ if __name__ == "__main__":
     print()
     for sv, ob in zip(test_sv, test_herm_patch):
         print(measure_sv(sv, ob))
+
+    test_img = jnp.stack([jnp.arange(32 * 32 * 1).reshape((32, 32)), jnp.arange(32 * 32 * 1).reshape((32, 32)),
+                          jnp.arange(32 * 32 * 1).reshape((32, 32))], axis=0)
+
+    print(test_img[:, :4, :4])
+
+    patches = extract_patches(test_img, patch_size=4, stride=2, padding=(0, 0))
+    print(patches.shape)
+    print(patches[0].shape)
+    print(patches[0])
+
+    test_params = jax.random.normal(shape=[3, 4 ** 2 - 1], key=jrng_key)
+    test_img = jnp.stack([jnp.arange(32*32*1).reshape((32, 32))]*3, axis = 0)
+    print(test_img.shape)
+    test_patches = extract_patches(test_img, patch_size=3, stride=1, padding=(1, 1))
+    print(test_patches.shape)
+    print(test_params.shape)
+    print(vmap_generate_2q_param_state(test_params))
+    test_out = vmap_single_kernel_op_through_extracted_patches(test_params, test_patches)
+    print(test_out.shape)
+    h = (32 - 3 + 1 * 2) // 1 + 1
+    print(h ** 2)
+
+    test_params2 = jax.random.normal(shape=[4, 3, 4 ** 2 - 1], key=jrng_key)
+    test_out2 = vmap_vmap_single_kernel_op_through_extracted_patches(test_params2, test_patches)
+    print(test_out2.shape)
+    test_out_2_features = test_out2.reshape((-1, 32, 32))
+    print(test_out_2_features.shape)
