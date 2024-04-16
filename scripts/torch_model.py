047baf5adc581ea1c079d274b79c88d0bc6a1c2f
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2024-04-16 18:31:28 +1000

    updates

--- scripts/torch_model.py
+++ scripts/torch_model.py
@@ -47,3 +47,48 @@ class FlippedQuanv3x3(nn.Module):
     def extra_repr(self) -> str:
         return f'in_channels={self.in_channels}, out_channels={self.out_channels}, stride={self.stride}, padding={self.padding}'
 
+class DataReUploadingLinear(nn.Module):
+    def __init__(self, in_dim, out_dim, n_qubits, n_reps):
+        super(DataReUploadingLinear, self).__init__()
+        assert 2 ** n_qubits >= out_dim
+        assert 4 ** n_qubits >= in_dim
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.n_qubits = n_qubits
+        self.n_reps = n_reps
+        self.pauli_string_tensor_list = generate_pauli_tensor_list(generate_nqubit_pauli_strings(n_qubits))
+        self.param_dim = 4 ** n_qubits - 1
+        self.params = torch.nn.Parameter(torch.randn((self.n_reps, self.param_dim)).type(COMPLEX_DTYPE))
+        self.bias = torch.nn.Parameter(torch.randn(out_dim).type(REAL_DTYPE))
+        self.observables = self.generate_observables()
+        self.pad_size = 4 ** n_qubits - self.in_dim
+
+    def extra_repr(self) -> str:
+        return f'in_dim={self.in_dim}, out_dim={self.out_dim}, n_qubits={self.n_qubits}, n_reps={self.n_reps}'
+
+    def generate_observables(self):
+        observables = []
+        for i in range(self.out_dim):
+            temp_bitstring = '{0:b}'.format(i).zfill(self.n_qubits)
+            ob = torch.outer(bitstring_to_state(temp_bitstring), bitstring_to_state(temp_bitstring))
+            observables.append(ob)
+        return torch.stack(observables)
+
+    def forward(self, x):
+        # x has size (batchsize, in_dim)
+        # pad x
+        x = torch.nn.functional.pad(x, (0, self.pad_size)).type(COMPLEX_DTYPE)
+        out = vmap_batch_linear_layer_func(x, self.params, self.pauli_string_tensor_list, self.observables,
+                                           self.n_qubits)
+        out = out.type(REAL_DTYPE) + self.bias
+        return out
+
+
+
+if __name__ == '__main__':
+    test_linear_module = DataReUploadingLinear(in_dim=45, out_dim=6, n_qubits=3, n_reps=10).to(DEVICE)
+    test_data = torch.randn((16, 45), device=DEVICE).type(COMPLEX_DTYPE)
+    print(test_data.shape)
+    test_out = test_linear_module(test_data.to(DEVICE))
+    print(test_out.shape)
+    print(test_linear_module)
