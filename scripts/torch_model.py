bc77702123e8b6469e7bb8ca608a3a5041cc6975
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2024-04-16 18:50:41 +1000

    updates

--- scripts/torch_model.py
+++ scripts/torch_model.py
@@ -83,12 +83,88 @@ class DataReUploadingLinear(nn.Module):
         out = out.type(REAL_DTYPE) + self.bias
         return out
 
+class HybridNet(nn.Module):
+    def __init__(self, in_channels, replacement_lvl):
+        super(HybridNet, self).__init__()
+        self.in_channels = in_channels
+        self.replacement_lvl = replacement_lvl
+        if self.replacement_lvl == 0:
+            self.layers = nn.Sequential(
+                nn.Conv2d(in_channels, 32, kernel_size=3),
+                nn.Conv2d(32, 16, kernel_size=3),
+                nn.Flatten(),
+                nn.Linear(16 * 28 * 28, 10)
+            )
+        elif self.replacement_lvl == 1:
+            self.layers = nn.Sequential(
+                FlippedQuanv3x3(in_channels=1, out_channels=32, stride=1, padding=None), # 32->30
+                FlippedQuanv3x3(in_channels=32, out_channels=16, stride=1, padding=None),  # 30->28
+                torch.nn.Flatten(),
+                nn.Linear(16 * 28 * 28, 10)
+            )
+        elif self.replacement_lvl == 2:
+            self.layers = torch.nn.Sequential(
+                FlippedQuanv3x3(in_channels=1, out_channels=32, stride=1, padding=None),  # 32->30
+                FlippedQuanv3x3(in_channels=32, out_channels=16, stride=1, padding=None),  # 30->28
+                torch.nn.Flatten(),
+                DataReUploadingLinear(16 * 28 * 28, 10, 7, 10)
+            )
+        else:
+            raise ValueError("replacement_lvl should be 0, 1, or 2")
 
+    def forward(self, x):
+        return self.layers(x)
 
 if __name__ == '__main__':
+    import torchvision
+    """
     test_linear_module = DataReUploadingLinear(in_dim=45, out_dim=6, n_qubits=3, n_reps=10).to(DEVICE)
     test_data = torch.randn((16, 45), device=DEVICE).type(COMPLEX_DTYPE)
     print(test_data.shape)
     test_out = test_linear_module(test_data.to(DEVICE))
     print(test_out.shape)
     print(test_linear_module)
+    """
+    preprocess = torchvision.transforms.Compose([
+        torchvision.transforms.Pad(2),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5,), (0.5,)),
+        torchvision.transforms.Lambda(lambda x: x.type(COMPLEX_DTYPE))
+    ])
+
+    train_dataset = torchvision.datasets.MNIST(
+        "MNIST",
+        train=True,
+        download=True,
+        transform=preprocess,
+    )
+    test_dataset = torchvision.datasets.MNIST(
+        "MNIST",
+        train=False,
+        download=True,
+        transform=preprocess,
+    )
+    dummy_trainloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=32, shuffle=True
+    )
+    dummy_testloader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=32, shuffle=True
+    )
+
+    dummy_x, dummy_y = next(iter(dummy_trainloader))
+
+    print(dummy_x.shape)  # 64x32x32
+    print(dummy_y.shape)  # 64
+    print(dummy_y)
+    print(dummy_x[0, 0, 16])
+
+    for rpl_lvl in [0,1,2]:
+        # full replacement need at lease A100-40GB
+        test_hybridmodel = HybridNet(in_channels=1, replacement_lvl=rpl_lvl).to(DEVICE)
+        print(test_hybridmodel)
+        if rpl_lvl == 0:
+            out = test_hybridmodel(dummy_x.type(REAL_DTYPE).to(DEVICE))
+        else:
+            out = test_hybridmodel(dummy_x.to(DEVICE))
+        print(out.shape)
+
